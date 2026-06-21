f8be8a0a909d4b3fcad868782818da025d0fe05c
James De Peña <jamesdepena0@gmail.com>
2026-06-21 12:35:08 -0400

    Crea el AppModule

---
+++ app/src/main/java/edu/ucne/dragonball_planets/di/AppModule.kt
@@ -0,0 +1,44 @@
+package edu.ucne.dragonball_planets.di
+
+import com.squareup.moshi.Moshi
+import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import edu.ucne.dragonball_planets.data.remote.DragonBallApi
+import edu.ucne.dragonball_planets.data.repository.PlanetRepositoryImpl
+import edu.ucne.dragonball_planets.domain.repository.PlanetRepository
+import retrofit2.Retrofit
+import retrofit2.converter.moshi.MoshiConverterFactory
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object AppModule {
+
+    @Provides
+    @Singleton
+    fun provideApi(moshi: Moshi): DragonBallApi {
+        return Retrofit.Builder()
+            .baseUrl("https://dragonball-api.com/api/")
+            .addConverterFactory(MoshiConverterFactory.create(moshi))
+            .build()
+            .create(DragonBallApi::class.java)
+    }
+
+    @Singleton
+    @Provides
+    fun provideMoshi(): Moshi {
+        return Moshi
+            .Builder()
+            .add(KotlinJsonAdapterFactory())
+            .build()
+    }
+
+    @Provides
+    @Singleton
+    fun provideRepository(api: DragonBallApi): PlanetRepository {
+        return PlanetRepositoryImpl(api)
+    }
+}
