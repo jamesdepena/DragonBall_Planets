645cc8ee93df4cb142aa909b2515f11e25632762
James De Peña <jamesdepena0@gmail.com>
2026-06-21 12:33:56 -0400

    Crea el dto de Planet

---
+++ app/src/main/java/edu/ucne/dragonball_planets/data/remote/dto/PlanetDto.kt
@@ -0,0 +1,22 @@
+package edu.ucne.dragonball_planets.data.remote.dto
+
+import edu.ucne.dragonball_planets.domain.model.Planet
+
+data class PlanetResponseDto(
+    val items: List<PlanetDto>
+)
+data class PlanetDto (
+    val id: Int,
+    val name: String,
+    val isDestroyed: Boolean,
+    val description: String,
+    val image: String,
+)  {
+    fun toDomain() = Planet(
+        id = id,
+        name = name,
+        isDestroyed = isDestroyed,
+        description = description,
+        image = image,
+    )
+}
