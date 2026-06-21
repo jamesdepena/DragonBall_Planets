0430f57b1ac4e1a4e365399ffbbce4af6c40e2b7
James De Peña <jamesdepena0@gmail.com>
2026-06-21 12:36:24 -0400

    Crea el DetailPlanetViewModel

---
+++ app/src/main/java/edu/ucne/dragonball_planets/presentation/detail/DetailPlanetViewModel.kt
@@ -0,0 +1,56 @@
+package edu.ucne.dragonball_planets.presentation.detail
+
+import androidx.lifecycle.SavedStateHandle
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import androidx.navigation.toRoute
+import dagger.hilt.android.lifecycle.HiltViewModel
+import edu.ucne.dragonball_planets.data.remote.Resource
+import edu.ucne.dragonball_planets.domain.usecase.GetPlanetDetailUseCase
+import edu.ucne.dragonball_planets.presentation.navigation.Screen
+import jakarta.inject.Inject
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
+import kotlinx.coroutines.launch
+
+@HiltViewModel
+class DetailPlanetViewModel @Inject constructor(
+    private val getPlanetDetailUseCase: GetPlanetDetailUseCase,
+    savedStateHandle: SavedStateHandle
+) : ViewModel() {
+
+    private val _state = MutableStateFlow(DetailPlanetUiState())
+    val state = _state.asStateFlow()
+
+    init {
+        val args = savedStateHandle.toRoute<Screen.PlanetDetail>()
+        loadPlanet(args.id)
+    }
+
+    private fun loadPlanet(id: Int) {
+        viewModelScope.launch {
+            _state.update { it.copy(isLoading = true) }
+
+            when (val result = getPlanetDetailUseCase(id)) {
+                is Resource.Success ->
+                    _state.update {
+                        it.copy(
+                            isLoading = false,
+                            planet = result.data
+                        )
+                    }
+
+                is Resource.Error ->
+                    _state.update {
+                        it.copy(
+                            isLoading = false,
+                            error = result.message
+                        )
+                    }
+
+                is Resource.Loading -> Unit
+            }
+        }
+    }
+}
