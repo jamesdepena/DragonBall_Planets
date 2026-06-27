8cce5e9aef026df46ae105eb8f2e03c8cfd4f16c
James De Peña <jamesdepena0@gmail.com>
2026-06-27 00:42:09 -0400

    Agrega modelo Character

---
+++ app/src/main/java/edu/ucne/dragonball_planets/domain/character/model/Character.kt
@@ -0,0 +1,12 @@
+package edu.ucne.dragonball_planets.domain.character.model
+
+data class Character(
+    val id: Int,
+    val name: String,
+    val ki: String,
+    val race: String,
+    val gender: String,
+    val description: String,
+    val image: String,
+    val maxKi: String,
+)
