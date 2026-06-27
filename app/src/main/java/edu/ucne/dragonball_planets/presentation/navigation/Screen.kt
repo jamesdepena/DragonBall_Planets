54e9a3d8b131cd96cb227c36fa51eca95a3f7d61
James De Peña <jamesdepena0@gmail.com>
2026-06-27 00:51:29 -0400

    Agrega las pantallas de los personajes

--- app/src/main/java/edu/ucne/dragonball_planets/presentation/navigation/Screen.kt
+++ app/src/main/java/edu/ucne/dragonball_planets/presentation/navigation/Screen.kt
@@ -8,4 +8,10 @@ sealed class Screen {
 
     @Serializable
     data class PlanetDetail(val id: Int) : Screen()
+
+    @Serializable
+    data object CharacterList : Screen()
+
+    @Serializable
+    data class CharacterDetail(val id: Int) : Screen()
 }
