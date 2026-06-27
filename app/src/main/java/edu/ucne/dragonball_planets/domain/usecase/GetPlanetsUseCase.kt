9aacc4442cc9b9db92f04f66b53ef0b0074bc7b8
James De Peña <jamesdepena0@gmail.com>
2026-06-26 22:49:27 -0400

    Quita suspend ya que retorna flow

--- app/src/main/java/edu/ucne/dragonball_planets/domain/usecase/GetPlanetsUseCase.kt
+++ app/src/main/java/edu/ucne/dragonball_planets/domain/usecase/GetPlanetsUseCase.kt
@@ -1,18 +1,14 @@
 package edu.ucne.dragonball_planets.domain.usecase
 
-import edu.ucne.dragonball_planets.data.remote.Resource
-import edu.ucne.dragonball_planets.data.remote.dto.PlanetDto
 import edu.ucne.dragonball_planets.domain.repository.PlanetRepository
-import jakarta.inject.Inject
+import javax.inject.Inject
 
 class GetPlanetsUseCase @Inject constructor(
     private val repository: PlanetRepository
 ) {
-    suspend operator fun invoke(
+    operator fun invoke(
         page: Int = 1,
         limit: Int = 10,
         name: String? = null
-    ): Resource<List<PlanetDto>> {
-        return repository.getPlanets(page, limit, name, null)
-    }
+    ) = repository.getPlanets(page, limit, name)
 }
