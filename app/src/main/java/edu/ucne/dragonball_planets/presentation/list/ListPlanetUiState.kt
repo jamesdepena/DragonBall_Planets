0597eeefc47d13c7443988c424ade8e4638757fe
James De Peña <jamesdepena0@gmail.com>
2026-06-26 22:50:36 -0400

    Usa Planet (dominio) en vez del DTO para la lista

--- app/src/main/java/edu/ucne/dragonball_planets/presentation/list/ListPlanetUiState.kt
+++ app/src/main/java/edu/ucne/dragonball_planets/presentation/list/ListPlanetUiState.kt
@@ -1,10 +1,10 @@
 package edu.ucne.dragonball_planets.presentation.list
 
-import edu.ucne.dragonball_planets.data.remote.dto.PlanetDto
+import edu.ucne.dragonball_planets.domain.model.Planet
 
-data class ListPlanetUiState (
+data class ListPlanetUiState(
     val isLoading: Boolean = false,
-    val planets: List<PlanetDto> = emptyList(),
+    val planets: List<Planet> = emptyList(),
     val error: String? = null,
     val filterName: String = "",
     val filterIsDestroyed: Boolean? = null
