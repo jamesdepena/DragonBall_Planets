157a64b421402a1967d853ad3d98e7acb74a3904
James De Peña <jamesdepena0@gmail.com>
2026-06-27 00:52:29 -0400

    Agrega la navegacion a las pantallas de los personajes y agrega el onDrawer para todas las pantallas

--- app/src/main/java/edu/ucne/dragonball_planets/presentation/navigation/NavHost.kt
+++ app/src/main/java/edu/ucne/dragonball_planets/presentation/navigation/NavHost.kt
@@ -1,34 +1,64 @@
 package edu.ucne.dragonball_planets.presentation.navigation
 
+import androidx.compose.material3.DrawerValue
+import androidx.compose.material3.rememberDrawerState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
-import edu.ucne.dragonball_planets.presentation.detail.DetailPlanetScreen
-import edu.ucne.dragonball_planets.presentation.planet_list.ListPlanetScreen
+import edu.ucne.dragonball_planets.presentation.character.list.ListCharacterScreen
+import edu.ucne.dragonball_planets.presentation.detail.DetailCharacterScreen
+import edu.ucne.dragonball_planets.presentation.planet.detail.DetailPlanetScreen
+import edu.ucne.dragonball_planets.presentation.planet.list.ListPlanetScreen
+import kotlinx.coroutines.launch
 
 @Composable
 fun NavHost(
     navHostController: NavHostController
 ) {
-    NavHost(
-        navController = navHostController,
-        startDestination = Screen.PlanetList
-    ) {
-        composable<Screen.PlanetList> {
-            ListPlanetScreen(
-                onPlanetClick = { planetId ->
-                    navHostController.navigate(Screen.PlanetDetail(planetId))
-                }
-            )
-        }
+    val scope = rememberCoroutineScope()
+    val drawerState = rememberDrawerState(initialValue = DrawerValue.Closed)
 
-        composable<Screen.PlanetDetail> {
-            DetailPlanetScreen(
-                onBack = {
-                    navHostController.navigateUp()
-                }
-            )
+    DrawerMenu(
+        drawerState = drawerState,
+        navHostController = navHostController
+    ) {
+        NavHost(
+            navController = navHostController,
+            startDestination = Screen.PlanetList
+        ) {
+            composable<Screen.PlanetList> {
+                ListPlanetScreen(
+                    onDrawer = { scope.launch { drawerState.open() } },
+                    onPlanetClick = { planetId ->
+                        navHostController.navigate(Screen.PlanetDetail(planetId))
+                    }
+                )
+            }
+
+            composable<Screen.PlanetDetail> {
+                DetailPlanetScreen(
+                    onDrawer = { scope.launch { drawerState.open() } },
+                    onBack = { navHostController.navigateUp() }
+                )
+            }
+
+            composable<Screen.CharacterList> {
+                ListCharacterScreen(
+                    onDrawer = { scope.launch { drawerState.open() } },
+                    onCharacterClick = { characterId ->
+                        navHostController.navigate(Screen.CharacterDetail(characterId))
+                    }
+                )
+            }
+
+            composable<Screen.CharacterDetail> {
+                DetailCharacterScreen(
+                    onDrawer = { scope.launch { drawerState.open() } },
+                    onBack = { navHostController.navigateUp() }
+                )
+            }
         }
     }
 }
